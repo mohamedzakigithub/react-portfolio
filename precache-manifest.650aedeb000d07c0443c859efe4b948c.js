2e7b6f0ec501b6c826dd31a7cc7e368a28f853c7
Mohamed Eleraky <mohamedzaki@hotmail.com>
2020-06-24 16:49:33 +1000

    Updates

---
+++ precache-manifest.650aedeb000d07c0443c859efe4b948c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a0db2c7c4ece03ab48a98d044da9be",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "5ff32e3b49121f55adda",
+    "url": "/employee-directory/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "8606b0350fe791044320",
+    "url": "/employee-directory/static/css/main.6a378c6f.chunk.css"
+  },
+  {
+    "revision": "5ff32e3b49121f55adda",
+    "url": "/employee-directory/static/js/2.ccc3e648.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/employee-directory/static/js/2.ccc3e648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8606b0350fe791044320",
+    "url": "/employee-directory/static/js/main.015afa64.chunk.js"
+  },
+  {
+    "revision": "9491d59d2eca79311dfc",
+    "url": "/employee-directory/static/js/runtime-main.d8db5b99.js"
+  },
+  {
+    "revision": "df724864e7dcf4e4555c4d7a052efb35",
+    "url": "/employee-directory/static/media/bg.df724864.jpg"
+  }
+]);
